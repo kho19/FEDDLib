ce99fa34d03d1f96f5144e411d8cd980d4d55380
Kyrill Ho <kyrillh@gmail.com>
2023-07-01 12:29:27 +0200

    Reformat and rework of assembly routines. No convergence.

--- feddlib/core/AceFemAssembly/specific/AssembleFENonLinLaplace_def.hpp
+++ feddlib/core/AceFemAssembly/specific/AssembleFENonLinLaplace_def.hpp
@@ -23,11 +23,11 @@ AssembleFENonLinLaplace<SC, LO, GO, NO>::AssembleFENonLinLaplace(
     tuple_disk_vec_ptr_Type tuple)
     : AssembleFE<SC, LO, GO, NO>(flag, nodesRefConfig, params, tuple) {
 
-  this->FEType_ = std::get<1>(this->diskTuple_->at(0));
-  this->dofs_ = std::get<2>(this->diskTuple_->at(0));
-  // Same as this->getNodesRefConfig().size();
-  this->numNodes_ = std::get<3>(this->diskTuple_->at(0));
-  this->dofsElement_ = this->numNodes_ * this->dofs_;
+    this->FEType_ = std::get<1>(this->diskTuple_->at(0));
+    this->dofs_ = std::get<2>(this->diskTuple_->at(0));
+    // Same as this->getNodesRefConfig().size();
+    this->numNodes_ = std::get<3>(this->diskTuple_->at(0));
+    this->dofsElement_ = this->numNodes_ * this->dofs_;
 }
 
 /*!
@@ -40,11 +40,12 @@ AssembleFENonLinLaplace<SC, LO, GO, NO>::AssembleFENonLinLaplace(
 template <class SC, class LO, class GO, class NO>
 void AssembleFENonLinLaplace<SC, LO, GO, NO>::assembleJacobian() {
 
-  SmallMatrixPtr_Type elementMatrix =
-      Teuchos::rcp(new SmallMatrix_Type(this->dofsElement_));
-  assemblyNonLinLaplacian(elementMatrix);
+    SmallMatrixPtr_Type elementMatrix =
+        Teuchos::rcp(new SmallMatrix_Type(this->dofsElement_));
+    assemblyNonLinLaplacian(elementMatrix);
 
-  this->jacobian_ = elementMatrix;
+    this->jacobian_ = elementMatrix;
+    elementMatrix->print();
 }
 
 /*!
@@ -58,62 +59,63 @@ template <class SC, class LO, class GO, class NO>
 void AssembleFENonLinLaplace<SC, LO, GO, NO>::assemblyNonLinLaplacian(
     SmallMatrixPtr_Type &elementMatrix) {
 
-  int dim = this->getDim();
-  int Grad = 0; // Needs to be fixed
-  UN deg = Helper::determineDegree(dim, this->FEType_, Grad);
+    int dim = this->getDim();
+    int Grad = 2; // Needs to be fixed
+    UN deg = Helper::determineDegree(dim, this->FEType_, Grad);
 
-  vec3D_dbl_ptr_Type dPhi;
-  vec2D_dbl_ptr_Type phi;
-  vec_dbl_ptr_Type weights = Teuchos::rcp(new vec_dbl_Type(0));
+    vec3D_dbl_ptr_Type dPhi;
+    vec2D_dbl_ptr_Type phi;
+    vec_dbl_ptr_Type weights = Teuchos::rcp(new vec_dbl_Type(0));
 
-  Helper::getDPhi(dPhi, weights, dim, this->FEType_, deg);
-  Helper::getPhi(phi, weights, dim, this->FEType_, deg);
+    Helper::getDPhi(dPhi, weights, dim, this->FEType_, deg);
+    Helper::getPhi(phi, weights, dim, this->FEType_, deg);
 
-  SC detB;
-  SC absDetB;
-  SmallMatrix<SC> B(dim);
-  SmallMatrix<SC> Binv(dim);
-  buildTransformation(B);
-  detB = B.computeInverse(Binv);
-  absDetB = std::fabs(detB);
-  vec3D_dbl_Type dPhiTrans(
-      dPhi->size(), vec2D_dbl_Type(dPhi->at(0).size(), vec_dbl_Type(dim, 0.)));
-  applyBTinv(dPhi, dPhiTrans, Binv);
+    SC detB;
+    SC absDetB;
+    SmallMatrix<SC> B(dim);
+    SmallMatrix<SC> Binv(dim);
+    buildTransformation(B);
+    detB = B.computeInverse(Binv);
+    absDetB = std::fabs(detB);
+    vec3D_dbl_Type dPhiTrans(
+        dPhi->size(),
+        vec2D_dbl_Type(dPhi->at(0).size(), vec_dbl_Type(dim, 0.)));
+    applyBTinv(dPhi, dPhiTrans, Binv);
 
-  vec_dbl_Type uLoc(weights->size(), -1.);
-  // At each quad node p_i gradient vector is duLoc[w] = [\partial_x phi,
-  // \partial_y phi]
-  vec2D_dbl_Type duLoc(weights->size(), vec_dbl_Type(dim, 0));
+    vec_dbl_Type uLoc(weights->size(), 0.);
+    // At each quad node p_i gradient vector is duLoc[w] = [\partial_x phi,
+    // \partial_y phi]
+    vec2D_dbl_Type duLoc(weights->size(), vec_dbl_Type(dim, 0));
 
-  // Build vector of current solution at quadrature nodes
-  for (int w = 0; w < phi->size(); w++) { // quadrature nodes
-    uLoc[w] = 0.;
-    for (int i = 0; i < phi->at(0).size();
-         i++) { // each basis function at the current quadrature node
-      uLoc[w] += (*this->solution_)[i] * phi->at(w).at(i);
-      for (int d = 0; d < dim; d++) {
-        duLoc[w][d] += (*this->solution_)[i] * dPhiTrans[w][i][d];
-      }
-    }
-  }
+    // Build vector of current solution at quadrature nodes
+    // quadrature nodes
+    /* for (int w = 0; w < phi->size(); w++) { */
+    /*     // each basis function at the current quadrature node */
+    /*     for (int i = 0; i < phi->at(0).size(); i++) { */
+    /*         uLoc[w] += (*this->solution_)[i] * phi->at(w).at(i); */
+    /*         for (int d = 0; d < dim; d++) { */
+    /*             duLoc[w][d] += (*this->solution_)[i] * dPhiTrans[w][i][d]; */
+    /*         } */
+    /*     } */
+    /* } */
 
-  // Build local stiffness matrix
-  Teuchos::Array<SC> value(1, 0.);
-  for (UN i = 0; i < this->numNodes_; i++) {
-    for (UN j = 0; j < this->numNodes_; j++) {
-      value[0] = 0.;
-      for (UN w = 0; w < weights->size(); w++) {
-        for (UN d = 0; d < dim; d++) {
-          value[0] += weights->at(w) *
-                      ((1 + uLoc[w] * uLoc[w]) * dPhiTrans[w][i][d] +
-                       2 * uLoc[w] * phi->at(w).at(i) * duLoc[w][d]) *
-                      dPhiTrans[w][j][d];
+    // Build local stiffness matrix
+    Teuchos::Array<SC> value(1, 0.);
+    for (UN i = 0; i < this->numNodes_; i++) {
+        for (UN j = 0; j < this->numNodes_; j++) {
+            value[0] = 0.;
+            for (UN w = 0; w < weights->size(); w++) {
+                for (UN d = 0; d < dim; d++) {
+                    value[0] += weights->at(w) *
+                                ((1 + uLoc[w] * uLoc[w]) * dPhiTrans[w][j][d] +
+                                 2 * uLoc[w] * phi->at(w).at(j) * duLoc[w][d]) *
+                                dPhiTrans[w][i][d];
+                }
+            }
+            value[0] *= absDetB;
+            (*elementMatrix)[i][j] = value[0];
         }
-      }
-      value[0] *= absDetB;
-      (*elementMatrix)[i][j] = value[0];
     }
-  }
 }
 
 /*!
@@ -126,75 +128,78 @@ void AssembleFENonLinLaplace<SC, LO, GO, NO>::assemblyNonLinLaplacian(
 template <class SC, class LO, class GO, class NO>
 void AssembleFENonLinLaplace<SC, LO, GO, NO>::assembleRHS() {
 
-  int dim = this->getDim();
-  int Grad = 2; // Needs to be fixed
-  UN deg = Helper::determineDegree(dim, this->FEType_, Grad);
+    int dim = this->getDim();
+    int Grad = 2; // Needs to be fixed
+    UN deg = Helper::determineDegree(dim, this->FEType_, Grad);
 
-  vec3D_dbl_ptr_Type dPhi;
-  vec2D_dbl_ptr_Type phi;
-  vec_dbl_ptr_Type weights = Teuchos::rcp(new vec_dbl_Type(0));
+    vec3D_dbl_ptr_Type dPhi;
+    vec2D_dbl_ptr_Type phi;
+    vec_dbl_ptr_Type weights = Teuchos::rcp(new vec_dbl_Type(0));
 
-  Helper::getDPhi(dPhi, weights, dim, this->FEType_, deg);
-  Helper::getPhi(phi, weights, dim, this->FEType_, deg);
+    Helper::getDPhi(dPhi, weights, dim, this->FEType_, deg);
+    Helper::getPhi(phi, weights, dim, this->FEType_, deg);
 
-  SC detB;
-  SC absDetB;
-  SmallMatrix<SC> B(dim);
-  SmallMatrix<SC> Binv(dim);
-  buildTransformation(B);
-  detB = B.computeInverse(Binv);
-  absDetB = std::fabs(detB);
-  vec3D_dbl_Type dPhiTrans(
-      dPhi->size(), vec2D_dbl_Type(dPhi->at(0).size(), vec_dbl_Type(dim, 0.)));
-  applyBTinv(dPhi, dPhiTrans, Binv);
-  vec_dbl_Type uLoc(weights->size(), -1.);
+    SC detB;
+    SC absDetB;
+    SmallMatrix<SC> B(dim);
+    SmallMatrix<SC> Binv(dim);
+    buildTransformation(B);
+    detB = B.computeInverse(Binv);
+    absDetB = std::fabs(detB);
+    vec3D_dbl_Type dPhiTrans(
+        dPhi->size(),
+        vec2D_dbl_Type(dPhi->at(0).size(), vec_dbl_Type(dim, 0.)));
+    applyBTinv(dPhi, dPhiTrans, Binv);
+    vec_dbl_Type uLoc(weights->size(), 0.);
 
-  // for now just const!
-  double x;
-  std::vector<double> paras0(1);
-  std::vector<double> valueFunc(dim);
-  // SC *paras = &(paras0[0]);
-  // this->rhsFunc_(&x, &valueFunc[0], paras);
+    // for now just const!
+    // double x;
+    // std::vector<double> paras0(1);
+    // std::vector<double> valueFunc(dim);
+    // SC *paras = &(paras0[0]);
+    // this->rhsFunc_(&x, &valueFunc[0], paras);
 
-  // Build vector of current solution at quadrature nodes
-  for (int w = 0; w < phi->size(); w++) { // quadrature nodes
-    uLoc[w] = 0.;
-    for (int i = 0; i < phi->at(0).size();
-         i++) { // each basis function at the current quadrature node
-      uLoc[w] += (*this->solution_)[i] * phi->at(w).at(i);
-    }
-  }
+    // Build vector of current solution at quadrature nodes
+    /* for (int w = 0; w < phi->size(); w++) { // quadrature nodes */
+    /*     for (int i = 0; i < phi->at(0).size(); */
+    /*          i++) { // each basis function at the current quadrature node */
+    /*         uLoc[w] += (*this->solution_)[i] * phi->at(w).at(i); */
+    /*     } */
+    /* } */
 
-  this->rhsVec_.reset(new vec_dbl_Type(this->dofsElement_, 0.));
-  // $fv$ term
-  Teuchos::Array<SC> valueOne(1, 0.);
-  // $(1 + u_0^2)\nabla u_0 \nabla v$ term
-  Teuchos::Array<SC> valueTwo(1, 0.);
-  // for storing matrix_column \cdot current_solution reduction operation
-  Teuchos::Array<SC> reductionValue(1, 0.);
-
-  // Build local stiffness matrx
-  for (UN i = 0; i < this->numNodes_; i++) {
-    reductionValue[0] = 0.;
-    for (UN j = 0; j < this->numNodes_; j++) { // Iterate test functions (rows)
-      valueOne[0] = 0.;
-      valueTwo[0] = 0.;
-      for (UN w = 0; w < dPhiTrans.size(); w++) { // Iterate quadrature nodes
-        valueOne[0] += weights->at(w) *
-                       phi->at(w).at(i); // Note that phi does not require
-                                         // transformation. absDetB suffices.
-        for (UN d = 0; d < dim; d++) {
-          valueTwo[0] += weights->at(w) * (1 + uLoc[w] * uLoc[w]) *
-                         dPhiTrans[w][i][d] * dPhiTrans[w][j][d];
+    this->rhsVec_.reset(new vec_dbl_Type(this->dofsElement_, 0.));
+    // $fv$ term
+    Teuchos::Array<SC> valueOne(1, 0.);
+    // $(1 + u_0^2)\nabla u_0 \nabla v$ term
+    Teuchos::Array<SC> valueTwo(1, 0.);
+    // for storing matrix_column \cdot current_solution reduction operation
+    Teuchos::Array<SC> reductionValue(1, 0.);
+    // TODO error could still be here!!
+    //  Build local stiffness matrx
+    for (UN i = 0; i < this->numNodes_; i++) {
+        reductionValue[0] = 0.;
+        // Iterate test functions (rows)
+        for (UN j = 0; j < this->numNodes_; j++) {
+            valueTwo[0] = 0.;
+            // Iterate quadrature nodes
+            for (UN w = 0; w < dPhiTrans.size(); w++) {
+                for (UN d = 0; d < dim; d++) {
+                    valueTwo[0] += weights->at(w) * (1 + uLoc[w] * uLoc[w]) *
+                                   dPhiTrans[w][i][d] * dPhiTrans[w][j][d];
+                }
+            }
+            // Perform reduction of current valueTwo and solution
+            reductionValue[0] += valueTwo[0] * (*this->solution_)[j];
         }
-      }
-      valueOne[0] *= absDetB * valueFunc[0];
-      valueTwo[0] *= absDetB;
-      // Perform reduction of current valueTwo and solution
-      reductionValue[0] = valueTwo[0] * (*this->solution_)[j];
+        reductionValue[0] *= absDetB;
+        valueOne[0] = 0.;
+        for (UN w = 0; w < dPhiTrans.size(); w++) {
+            // Note that phi does not require transformation. absDetB suffices.
+            valueOne[0] += weights->at(w) * phi->at(w).at(i);
+        }
+        valueOne[0] *= absDetB; // constant rhs of one (* valueFunc[0];)
+        (*this->rhsVec_)[i] = valueOne[0] - reductionValue[0];
     }
-    (*this->rhsVec_)[i] = valueOne[0] - reductionValue[0];
-  }
 }
 /*!
 
@@ -208,33 +213,34 @@ template <class SC, class LO, class GO, class NO>
 void AssembleFENonLinLaplace<SC, LO, GO, NO>::buildTransformation(
     SmallMatrix<SC> &B) {
 
-  TEUCHOS_TEST_FOR_EXCEPTION((B.size() < 2 || B.size() > 3), std::logic_error,
-                             "Initialize SmallMatrix for transformation.");
-  UN index;
-  UN index0 = 0;
-  for (UN j = 0; j < B.size(); j++) {
-    index = j + 1;
-    for (UN i = 0; i < B.size(); i++) {
-      B[i][j] = this->nodesRefConfig_.at(index).at(i) -
-                this->nodesRefConfig_.at(index0).at(i);
+    TEUCHOS_TEST_FOR_EXCEPTION((B.size() < 2 || B.size() > 3), std::logic_error,
+                               "Initialize SmallMatrix for transformation.");
+    UN index;
+    UN index0 = 0;
+    for (UN j = 0; j < B.size(); j++) {
+        index = j + 1;
+        for (UN i = 0; i < B.size(); i++) {
+            B[i][j] = this->nodesRefConfig_.at(index).at(i) -
+                      this->nodesRefConfig_.at(index0).at(i);
+        }
     }
-  }
 }
 
 template <class SC, class LO, class GO, class NO>
 void AssembleFENonLinLaplace<SC, LO, GO, NO>::applyBTinv(
     vec3D_dbl_ptr_Type &dPhiIn, vec3D_dbl_Type &dPhiOut,
     SmallMatrix<SC> &Binv) {
-  UN dim = Binv.size();
-  for (UN w = 0; w < dPhiIn->size(); w++) {
-    for (UN i = 0; i < dPhiIn->at(w).size(); i++) {
-      for (UN d1 = 0; d1 < dim; d1++) {
-        for (UN d2 = 0; d2 < dim; d2++) {
-          dPhiOut[w][i][d1] += dPhiIn->at(w).at(i).at(d2) * Binv[d2][d1];
+    UN dim = Binv.size();
+    for (UN w = 0; w < dPhiIn->size(); w++) {
+        for (UN i = 0; i < dPhiIn->at(w).size(); i++) {
+            for (UN d1 = 0; d1 < dim; d1++) {
+                for (UN d2 = 0; d2 < dim; d2++) {
+                    dPhiOut[w][i][d1] +=
+                        dPhiIn->at(w).at(i).at(d2) * Binv[d2][d1];
+                }
+            }
         }
-      }
     }
-  }
 }
 
 } // namespace FEDD
