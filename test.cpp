d5fce9c64ed6c872011fdbadfbdaf0e63b7ad7fa
kyrillh <kyrillh@gmail.com>
2024-04-11 19:09:11 +0200

    Test linter

---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  std::cout << "this is going to be a crasy long line of code that should be formated by clang-format###################L####################################################################################################" << std::endl;
+  return 0;
+}
