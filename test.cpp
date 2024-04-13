48b04c1f8bd064fd3ddcc0e5a333e33bc28e3953
kyrillh <kyrillh@gmail.com>
2024-04-13 12:16:01 +0200

    7
    nothing

--- test.cpp
+++ test.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "this is going to be a crasy long line of code that should be "
-               "formated by "
-               "clang-format###################L###############################"
-               "######################################"
-               "###############################"
-               "clang-format###################L###############################"
-               "######################################"
-               "###############################"
-            << std::endl;
-  return 0;
+    std::cout << "this is going to be a crasy long line of code that should be "
+                 "formated by "
+                 "###############################"
+              << std::endl;
+    return 0;
 }
