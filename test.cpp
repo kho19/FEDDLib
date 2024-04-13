3555ad1400daacbef541a39cdecca549122ca05c
kyrillh <kyrillh@gmail.com>
2024-04-13 12:46:26 +0200

    8

--- test.cpp
+++ test.cpp
@@ -3,7 +3,6 @@
 int main(int argc, char *argv[]) {
     std::cout << "this is going to be a crasy long line of code that should be "
                  "formated by "
-                 "###############################"
               << std::endl;
     return 0;
 }
